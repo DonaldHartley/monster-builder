4dab8d2ce594588a8d13b4d12e31cef4b9edfd5b
DonaldHartley <Donald.H.Hartley@gmail.com>
2020-03-29 13:37:00 -0500

    configured database connection and tested connection

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,14 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'monster',
+        loadChildren: () => import('./monster/monster.module').then(m => m.MonsterbuilderMonsterModule)
+      },
+      {
+        path: 'basetype',
+        loadChildren: () => import('./basetype/basetype.module').then(m => m.MonsterbuilderBasetypeModule)
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
