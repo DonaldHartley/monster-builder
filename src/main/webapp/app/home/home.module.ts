9229c5a59ca15db04ea1ace3ced0feb272d45162
DonaldHartley <Donald.H.Hartley@gmail.com>
2020-03-29 15:37:44 -0500

    cleaned up bad test on home page

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,10 +4,9 @@ import { RouterModule } from '@angular/router';
 import { MonsterbuilderSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
-import { MonsterComponent } from 'app/entities/monster/monster.component';
 
 @NgModule({
   imports: [MonsterbuilderSharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent, MonsterComponent]
+  declarations: [HomeComponent]
 })
 export class MonsterbuilderHomeModule {}
